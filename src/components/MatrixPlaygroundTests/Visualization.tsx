de99fbcbf2fbbb90260c05bc19523a150c358267
ShemaiahYaba <67932039+ShemaiahYaba@users.noreply.github.com>
2025-03-22 16:21:31 +0100

    refactor: Simplify ArrowHelper instantiation by removing unused useRef and redundant code

--- src/components/MatrixPlaygroundTests/Visualization.tsx
+++ src/components/MatrixPlaygroundTests/Visualization.tsx
@@ -1,6 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { useRef } from "react";
-import { ArrowHelper, Vector3 } from "three";
+import { Vector3, ArrowHelper } from "three";
 
 const EigenVectorVisualization = ({
   eigenvectors,
@@ -15,20 +14,11 @@ const EigenVectorVisualization = ({
         const dir = new Vector3(vec[0], vec[1], vec.length > 2 ? vec[2] : 0)
           .clone()
           .normalize();
-        const arrowRef = useRef<ArrowHelper | null>(null);
 
         return (
           <primitive
             key={index}
-            object={
-              new ArrowHelper(
-                dir, // Direction
-                new Vector3(0, 0, 0), // Origin
-                2, // Length
-                0x00000 // Color
-              )
-            }
-            ref={arrowRef}
+            object={new ArrowHelper(dir, new Vector3(0, 0, 0), 2, 0xff0000)}
           />
         );
       })}
