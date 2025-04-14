e35452d8e0dd141120c0e7ef0c161c75f9f7998e
ShemaiahYaba <67932039+ShemaiahYaba@users.noreply.github.com>
2025-04-14 11:30:13 +0100

    fix: Add dark mode support for FeatureCard component

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -17,9 +17,9 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
   return (
     <a
       href={href}
-      className="flex flex-col items-center justify-center px-14 py-10 bg-gray-100 rounded-lg shadow-md hover:shadow-lg"
+      className="flex flex-col items-center justify-center px-14 py-10 bg-gray-100 rounded-lg shadow-md hover:shadow-lg dark:bg-gray-900"
     >
-      <div className="text-4xl">
+      <div className="text-4xl dark:text-gray-300">
         <Icon />
       </div>
       <h3 className="mt-4 text-lg font-semibold text-center">{name}</h3>
